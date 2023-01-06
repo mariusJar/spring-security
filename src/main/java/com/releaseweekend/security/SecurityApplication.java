6df5a372c3a1eceada704e1919642078c0f6730d
mariusJar <marius.jaraminas@gmail.com>
2023-01-06 12:15:23 +0200

    Bank accounts CRUD app with basic authentication

--- src/main/java/com/releaseweekend/security/SecurityApplication.java
+++ src/main/java/com/releaseweekend/security/SecurityApplication.java
@@ -2,9 +2,8 @@ package com.releaseweekend.security;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
 
-@SpringBootApplication(exclude = SecurityAutoConfiguration.class)
+@SpringBootApplication
 public class SecurityApplication {
 
 	public static void main(String[] args) {
--- src/main/java/com/releaseweekend/security/services/AccountService.java
+++ src/main/java/com/releaseweekend/security/services/AccountService.java
@@ -6,7 +6,6 @@ import org.springframework.stereotype.Service;
 import java.math.BigDecimal;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 import java.util.stream.Collectors;
 
 @Service
